987813f0017a8fd51f499b080a954b66ee2781cc
unknown <>
2016-03-07 20:11:30 +0800

    initial commit

---
+++ Application/View/Controller/ChengjiController.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace View\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+
+    public function index()
+    {
+        $openid = I('get.openid', '');
+        if ($openid == '') {
+            $this->error('请在微信中点击自动回复的链接打开本页面！');
+        }
+        $this->openid = $openid;
+        $this->display();
+    }
+}
