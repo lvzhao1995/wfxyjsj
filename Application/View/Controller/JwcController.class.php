d586022fbb937181b152d46042b3a0dc47a11300
lvzhao1995 <abc-1.2@qq.com>
2016-03-21 16:42:19 +0800

    删除重复的方法

--- Application/View/Controller/JwcController.class.php
+++ Application/View/Controller/JwcController.class.php
@@ -274,12 +274,4 @@ class JwcController extends PublicController
         $this->title="选修课查询";
         $this->display();
     }
-    
-    public function _empty($name){
-        $openid = I('get.openid');
-        if ($openid == '') {
-            $this->error('请在微信中点击自动回复的链接打开本页面！');
-        }
-        $this->redirect('Index/index', array('openid' => $openid), 0);
-    }
 }
